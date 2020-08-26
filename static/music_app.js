155ef9fa79ae82c229995d1231777eef2a158800
Bryan Green <bcgreen@gmail.com>
2020-08-26 10:41:48 -0500

    adding progress bar

--- static/music_app.js
+++ static/music_app.js
@@ -1,19 +1,41 @@
 let song_index = 0;
 let playlist = [];
 let player = new Audio();
+let bar_width = 0;
+let current_bar_width = 0;
+let timer;
 
 $(function () {
 
+    $('div#play_pos').mousedown(function (event) {
+        var relX = event.pageX - $(this).offset().left;
+        current_bar_width = relX;
+        const num = Math.floor(relX);
+        player.currentTime = scale(num, 0, 350, 0, player.duration);
+    });
+
+    player.addEventListener('loadedmetadata', () => {
+        $('span#song_duration').html(convert_duration(player.duration));
+        bar_width = document.getElementById('play_pos').offsetWidth / player.duration;
+        timer = setInterval(advance_progress_bar, 1000);
+    });
+
     player.onended = function () {
+        clearInterval(timer);
         if (document.getElementById("autoplay").checked) {
+            player.src = "";
             song_index += 1;
             player.src = playlist[song_index];
-            player.play();
+            $('a.music_link').removeClass("active");
+            // $('a.music_link')[song_index].add("active");
             $('span#song_name').text(playlist[song_index]);
+            current_bar_width = 0;
+            bar_width = 0;
+            player.play();
         }
     };
 
-    player.ontimeupdate = function(){
+    player.ontimeupdate = function () {
         document.getElementById("song_position").innerHTML = convert_duration(player.currentTime);
     };
 
@@ -23,6 +45,7 @@ $(function () {
                 playlist.push(data.path + data.songs[i]);
             }
         });
+
 
     $('button#play_pause').bind('click', function () {
         if (this.innerText === 'Pause') {
@@ -37,14 +60,29 @@ $(function () {
     $('a.music_link').bind('click', function () {
         player.src = "";
         player.src = this.id;
+        clearInterval(timer);
+        $('a.music_link').removeClass("active");
+        this.classList.add("active");
         $('span#song_name').text(this.id);
         song_index = playlist.indexOf(this.id);
+        current_bar_width = 0;
+        bar_width = 0;
         player.play();
-        player.addEventListener('loadedmetadata', () => {
-            $('span#song_duration').html(convert_duration(player.duration));
-        });
+
     });
 });
+
+function advance_progress_bar() {
+    current_bar_width += bar_width;
+    if(current_bar_width <= $('#play_pos').width()){
+        $('div#play_bar').width(current_bar_width);
+    }
+
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 function convert_duration(song_duration) {
     let sec_str;
